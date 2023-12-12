817aa3ecd8eefb5f7030a1dad3277b185b952b8a
Naveen Sirivella <naveensalesforce.learning70@gmail.com>
2023-12-12 20:44:33 +0530

    updated the files

--- src/main/java/com/qa/opencart/utils/AppConstants.java
+++ src/main/java/com/qa/opencart/utils/AppConstants.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class AppConstants {
 	
-	public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login";
+	public static final String LOGIN_PAGE_TITLE_VALUE = "Account Login11";
 	public static final String ACCOUNTS_PAGE_TITLE_VALUE = "My Account";
 	public static final String LOGIN_PAGE_URL_FRACTION_VALUE = "route=account/login";
 	
